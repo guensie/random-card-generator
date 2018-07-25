0e42c44ffd1623bd99296695eed139a558e9d991
Donzalee Baker <jdonzalee@gmail.com>
2018-07-25 22:58:51 +0000

    index.js updated

--- vanillajs-hello/src/js/index.js
+++ vanillajs-hello/src/js/index.js
@@ -7,9 +7,29 @@ import $ from "jquery";
 //include bootstrap npm library into the bundle
 import 'bootstrap';
 //include your own styles
-import '../style/index.scss';
+import '../style/styles.scss';
 
-window.onload = function(){
-    console.log('Hello Rigo from the console!');
-    document.querySelector('.error').style.display = "none";
+window.onload = () => {
+  document.querySelector('.card').classList.add(generateRandomSuit());
+  document.querySelector('.card').innerHTML = (generateRandomNumber());
+
+
 };
+
+let generateRandomNumber = () => {
+    let numbers = ['A', '2', '3', '4', '5', '6', '7','8','9', 'J','Q','K'];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+    let suit =['Diamond', 'Hearts', 'Spade', 'Clubs'];
+    let indexSuit = Math.floor(Math.random() * numbers.length);
+     return suits[indexSuit];
+       
+};
+
+   
+    
+
+    
